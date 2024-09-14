0fb53b7c3280ddf3cb788d8d05b85208efabea23
Marlen Satriani <marlen7891011@gmail.com>
2024-09-14 22:56:38 +0700

    Start

---
+++ src/components/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>Aku suka msewing dan asawdku sigma</h1>
+            <h1>Aku suka msewing dan asawdku sigma</h1>
+        </div>
+    );
+};
+
+export default About;
